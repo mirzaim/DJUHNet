5033810f5e4453f5d83f4efe260e18c89dcb8027
Morteza Mirzai <mortezamirzai120@gmail.com>
2023-01-31 17:27:54 +0330

    Update swinir to use representation learning.

--- models/select_network.py
+++ models/select_network.py
@@ -202,6 +202,23 @@ def define_G(opt):
                    resi_connection=opt_net['resi_connection'])
 
     # ----------------------------------------
+    # SwinIR Image Retrieval
+    # ----------------------------------------
+    elif net_type == 'swinir_img_rtval':
+        from models.network_swinir_img_rtval import SwinIR as net
+        netG = net(upscale=opt_net['upscale'],
+                   in_chans=opt_net['in_chans'],
+                   img_size=opt_net['img_size'],
+                   window_size=opt_net['window_size'],
+                   img_range=opt_net['img_range'],
+                   depths=opt_net['depths'],
+                   embed_dim=opt_net['embed_dim'],
+                   num_heads=opt_net['num_heads'],
+                   mlp_ratio=opt_net['mlp_ratio'],
+                   upsampler=opt_net['upsampler'],
+                   resi_connection=opt_net['resi_connection'])
+
+    # ----------------------------------------
     # VRT
     # ----------------------------------------
     elif net_type == 'vrt':
